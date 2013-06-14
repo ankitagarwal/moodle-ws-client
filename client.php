30b977eeb3bbc531b9e26010889a30ba71ea10ac
Ankit Agarwal <ankit@moodle.com>
2013-06-14 13:45:52 +0800

    Creating base class and a sample xmlrpc client

---
+++ client.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Webservice test client for Moodle
+ * @author Ankit Agarwal
+ *
+ */
+class wsclient {
+    protected $token;
+    protected $domain;
+    public $func;
+    protected $debug;
+    public $prot;
+    protected $debugdata;
+    protected $curl;
+
+    function __construct($token, $domain, $debug = true){
+        require_once('curl.php');
+        $this->token = $token;
+        $this->domain = $domain;
+        $this->debug = $debug;
+        $this->curl = new curl;
+        if ($this->debug) {
+            @ini_set('display_errors', '1');
+            error_reporting(-1);
+        }
+    }
+    function wscall ($func, $params) {
+        header('Content-Type: text/plain');
+        $post = $this->callwsapi($func, $params);
+        if ($this->debug) {
+            echo $this->debugdata;
+        }
+    }
+    function callwsapi ($func, $params) {
+        // Child classes must extend.
+        return true;
+    }
+}
+
+class xmlrpc extends wsclient {
+    public $prot = "XMLRPC";
+    function callwsapi($func, $params) {
+
+        $serverurl = $this->domain . '/webservice/xmlrpc/server.php'. '?wstoken=' . $this->token;
+        $post = xmlrpc_encode_request($func, array($params));
+        $result = $this->curl->post($serverurl, $post);
+        $result = xmlrpc_decode($result);
+
+        $this->debugdata = "<pre>".PHP_EOL.
+            "Serverurl: $serverurl".PHP_EOL.
+            "Params:".PHP_EOL.
+            print_r($params, true).
+            PHP_EOL."Result:".
+            print_r($result, true).
+            PHP_EOL."</pre>";
+
+        return $result;
+
+    }
+}
+
+$client = new xmlrpc("ebd1167dbf865ac5186c3157469f5764", "http://ankit.moodle.local/stable/master/moodle/");
+$params = array (
+                'roleid' => 3,
+                'userid' => 2,
+                'contextlevel' => "course",
+                'instanceid' => 2
+                );
+
+$client->wscall("core_role_assign_roles", array($params));
